c79b86f6250bc95664b5bcb114e1fb94af688116
volkanyasiryilmaz <volkanyasiryilmaz@hotmail.com>
2022-02-07 19:18:28 +0300

    everything is ok, there is a problem with the translation

--- index.php
+++ index.php
@@ -1,33 +1,23 @@
 <?php
-
+//MORSE ALPHAHET
 $morse=["-----",".----","..---","...--","....-",".....","-....","--...","---..","----.", //SAYILAR    
 ".-.-.-","--..--","..--..",".----.","-.-.--","-..-.",//NOKTALAMA İŞARETLERİ
 ".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--",
 "-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..",
 "-.-..","--.-.",".-..-","---.",".--..","..--" //harfler
-
 ];
-$alpahet=[
+//LATIN ALPHABET
+$alphabet=[
     "0","1","2","3","4","5","6","7","8","9",
     ".",",","?","'","-","!","/",
     "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X",
 "Y","Z","Ç","Ğ","İ","Ö","Ş","Ü"];
-
-
-echo "<pre>";
-print_r($morse);
-print_r($alpahet);
-echo "</pre>";
-
-$sonuc=str_replace($morse,$alpahet,"-.... --... ....- -.... ..... -.... ---.. ....-");
-echo $sonuc;
-
-
-
-
-
-
-echo "<br/> <br/>";
+//Morse to Latin
+function translate($morse,$alphabet, $will_be_translated){
+  $translated=str_replace($morse, $alphabet, $will_be_translated);
+  return $translated;
+}
+//API REQUEST WITH CURL
 $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL,"http://ik.olleco.net/morse-api/");
@@ -38,21 +28,35 @@ curl_setopt($ch, CURLOPT_POSTFIELDS,
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $server_output = curl_exec($ch);
-// echo $server_output;
 
-$server_output=json_decode($server_output,true);
-echo "<pre>";
-print_r($server_output);
-echo "</pre>";
-echo "<br/><br/>";
-$key1=array_keys($server_output)[0];
-$key2=array_keys($server_output)[1];
+$server_output_array=json_decode($server_output,true); // convert api to json format
+$key1=array_keys($server_output_array)[0];
+$key2=array_keys($server_output_array)[1];
+$key2_in=($server_output_array)[$key2][0];
+$key3=array_keys($key2_in)[0];
+$key4=array_keys($key2_in)[1];
+$key5=array_keys($key2_in)[2];
+$key5_in=$key2_in[$key5];
+$key6=array_keys($key5_in)[0];
+$key7=array_keys($key5_in)[1];
+$key8=array_keys($key5_in)[2];
+$key9=array_keys($key5_in)[3];
+$key10=array_keys($key5_in)[4];
 
+$will_be_translated = [
+$server_output_array[$key1],
+$key2_in[$key3],
+$key2_in[$key4],
+$key5_in[$key6],
+$key5_in[$key7],
+$key5_in[$key8],
+$key5_in[$key9],
+$key5_in[$key10]
+];
 
+$latin = translate($morse,$alphabet,$will_be_translated);
 curl_close ($ch);
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -71,57 +75,57 @@ curl_close ($ch);
             <th scope="col">#</th>
             <th scope="col">Data</th>
             <th scope="col">Morse</th>
-            <th scope="col">Convert to Latin</th>
+            <th scope="col">Latin</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <th scope="row">1</th>
             <td><?php echo $key1;?></td>
-            <td>-.... --... ....- -.... ..... -.... ---.. ....-</td>
-            <td>67465684</td>
+            <td><?php echo $server_output_array[$key1]; ?></td>
+            <td><?php echo $latin[0];?></td>
           </tr>
           <tr>
             <th scope="row">2</th>
-            <td>model</td>
-            <td>.- -- -.. / .-. -.-- --.. . -. / ----. / ...-- ----. ----- ----- / .---- ..--- -....- -.-. --- .-. . / .--. .-. --- -.-. . ... ... --- .-.</td>
-            <td>amd ryzen 9 3900 12-core processor</td>
+            <td><?php echo $key3;?> </td>
+            <td><?php echo $key2_in[$key3];?><td>
+            <td><?php echo $latin[1];?></td>
           </tr>
           <tr>
             <th scope="row">3</th>
-            <td>speed</td>
-            <td>..--- .---- ----. .....</td>
-            <td>2195</td>
+            <td><?php echo $key4;?></td>
+            <td><?php echo $key2_in[$key4]; ?></td>
+            <td><?php echo $latin[2];?></td>
           </tr>
           <tr>
             <th scope="row">4</th>
-            <td>user</td>
-            <td>.---- ....- ---.. ..--- ----. -.... ----- -----</td>
-            <td>14829600</td>
+            <td><?php echo $key6;?></td>
+            <td><?php echo $key5_in[$key6]; ?></td>
+            <td><?php echo $latin[3];?></td>
           </tr>
           <tr>
             <th scope="row">5</th>
-            <td>nice</td>
-            <td>-----</td>
-            <td>0</td>
+            <td><?php echo $key7;?></td>
+            <td><?php echo $key5_in[$key7];?></td>
+            <td><?php echo $latin[4];?></td>
           </tr>
           <tr>
             <th scope="row">6</th>
-            <td>sys</td>
-            <td>-----</td>
-            <td>0</td>
+            <td><?php echo $key8;?></td>
+            <td><?php  echo $key5_in[$key8]; ?></td>
+            <td><?php echo $latin[5];?></td>
           </tr>
           <tr>
             <th scope="row">7</th>
-            <td>idle</td>
-            <td>....- ..--- .---- ----. ..... -.... ....- ...-- ..... -.... ----- -----</td>
-            <td>421956435600</td>
+            <td><?php echo $key9; ?></td>
+            <td><?php echo $key5_in[$key9]; ?></td>
+            <td><?php echo $latin[6];?></td>
           </tr>
           <tr>
             <th scope="row">8</th>
-            <td>irq</td>
-            <td>-----</td>
-            <td>0</td>
+            <td><?php echo $key10; ?></td>
+            <td><?php echo $key5_in[$key10]; ?> </td>
+            <td><?php echo $latin[7];?></td>
           </tr>
           
         </tbody>
